10c84cbecc6980b98d0eaa347c86c153ed8b5fce
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-01-26 16:55:31 +0000

    contact avaliability basic added

---
+++ pages/contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col items-center">
+      <div class="text-center border-t-4 border-b-4 border-blue-600 rounded-xl px-2 py-4">
+          <p class="text-2xl">STATUS</p>
+          <p class="text-4xl text-green-600">AVAILABLE</p>
+          <p class="text-gray-700">I am Currently taking on new projects</p>
+      </div>
+      
+      <div>
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
