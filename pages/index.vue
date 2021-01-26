cdeb126e17d02ab44c9bd6c79b4e27978360d5b3
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-01-26 15:54:01 +0000

    basic index page design

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <p>something in the middle</p>
+  <div class="flex flex-col items-center">
+    <div class="text-center">
+      <p class="text-4xl">Ethan Lundregan</p>
+      <p>A Next Generation Developer</p>
+    </div>
+    <div class="flex mt-8">
+      <button class="text-white bg-blue-600 rounded m-2 p-2 shadow-md">View My Work</button>
+      <button class="text-white bg-blue-600 rounded m-2 p-2 shadow-md">Get In Touch</button>
+    </div>
   </div>
 </template>
 
