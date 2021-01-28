d7bcf36cc964e5867d8ce3e44c520f81d5e838f1
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-01-28 17:21:05 +0000

    removed name adjusted colors for darkmode

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,14 @@
 <template>
   <footer class="text-center font-serif flex flex-col items-center">
-    <p class="font-sans">Ethan Lundregan</p>
     <div class="flex justify-around w-64 pb-2">
-      <a href="https://github.com/lundregan" target="_blank">
-        <font-awesome-icon :icon="['fab', 'github']" class="text-purple-600 text-2xl grow" />
-      </a>
       <a href="https://twitter.com/lundregan1" target="_blank">
-        <font-awesome-icon :icon="['fab', 'twitter']" class="text-blue-600 text-2xl grow" />
+        <font-awesome-icon :icon="['fab', 'twitter']" class="text-blue-600 dark:text-blue-500 text-2xl grow" />
+      </a>
+      <a href="https://github.com/lundregan" target="_blank">
+        <font-awesome-icon :icon="['fab', 'github']" class="text-purple-600 dark:text-purple-500 text-2xl grow" />
       </a>
       <a href="https://linkedin.com/in/elundregan/" target="_blank">
-        <font-awesome-icon :icon="['fab', 'linkedin']" class="text-blue-700 text-2xl grow" />
+        <font-awesome-icon :icon="['fab', 'linkedin']" class="text-blue-700 dark:text-blue-500 text-2xl grow" />
       </a>
     </div>
   </footer>
