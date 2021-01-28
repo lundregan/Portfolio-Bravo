c3c4a821896ad9a0f71cbb70a4d298394c84e4b9
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-01-28 17:22:21 +0000

    icon style chnage on darkmode toggle

--- components/DarkmodeToggle.vue
+++ components/DarkmodeToggle.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    <font-awesome-icon :icon="['far', 'lightbulb']" class="text-orange-700 dark:text-gray-400 text-2xl grow" @click="toggleTheme" />
+    <font-awesome-icon :icon="[iconStyle, 'lightbulb']" class="text-orange-500 dark:text-gray-500 text-2xl grow" @click="toggleTheme" />
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex"
+
 export default {
+  data () {
+    return {
+      iconStyle: 'far'
+    }
+  },
+  computed: {
+    ...mapGetters({ theme: "getTheme" }),
+  },
+  watch: {
+    theme(theme) {
+      this.iconStyle = (theme == 'dark') ? 'far' : 'fas' 
+    }
+  },
   methods: {
     toggleTheme() {
       this.$store.dispatch("toggleTheme");
