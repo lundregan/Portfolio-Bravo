66ebcc50fdde8b019f6f27766df187360eb38f12
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-01-29 16:06:43 +0000

    basic blog setup similiar to projects, using content and dynamic routes

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div class="bg-gray-200 w-1/2 bg-purple-600 rounded-t-xl">
+      <p class="text-center text-white py-1 text-xl">{{blogPost.title}}</p>
+      <div class="bg-gray-200 rounded-t-xl p-8">
+        <nuxt-content :document="blogPost"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({$content, params, error}) {
+    try {
+      const blogPost = await $content('blog', params.slug).fetch()
+
+      return { blogPost }
+    } catch (err) {
+      error({
+        statusCode: 404,
+        message: 'Blog Post Page not found',
+      })
+    }
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <div class="w-1/2">
+      <div v-for="post in posts" :key="post.id" class="bg-gray-100 p-2 shadow-md">
+        <NuxtLink :to="`blog/${post.link}`">
+          <div class="flex justify-between">
+            <p>{{post.title}}</p>
+            <p class="italic">{{post.date}}</p>
+          </div>
+          <p class="p-2">{{post.synopsis}}</p>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const posts = await $content('blog').sortBy('createdAt').fetch()
+
+    return { posts }
+  }
+}
+</script>
+
+<style>
+
+</style>
