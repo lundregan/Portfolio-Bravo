6e3222837a94be6db1df971cd1bd59ca4e53b726
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-02-02 14:49:17 +0000

    margin psaced away from footer

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center items-center">
-    <div class="bg-gray-200 w-1/2 bg-blue-600 dark:bg-purple-600 rounded-t-xl text-black">
+    <div class="bg-gray-200 w-1/2 bg-blue-600 dark:bg-purple-600 rounded-t-xl text-black mb-8">
       <p class="text-center text-white py-1 text-xl">{{blogPost.title}}</p>
       <div class="bg-gray-200 rounded-t-xl p-8 flex justify-center">
         <nuxt-content :document="blogPost" class="prose text-center"/>
