00bf682d958f3ea564e23501a5715754d41d61d5
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-02-03 16:04:02 +0000

    added shadow

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-center items-center">
-    <div class="mx-4 bg-gray-200 w-full lg:w-1/2 bg-blue-600 dark:bg-purple-600 rounded-t-xl text-black mb-8">
+  <div class="flex justify-center items-center pt-8">
+    <div class="mx-4 bg-gray-200 w-full lg:w-1/2 bg-blue-600 dark:bg-purple-600 rounded-t-xl text-black mb-8 shadow">
       <p class="text-center text-white py-1 text-xl">{{blogPost.title}}</p>
       <div class="bg-gray-200 rounded-t-xl p-8 flex justify-center">
         <nuxt-content :document="blogPost" class="prose text-center text-sm lg:text-base"/>
