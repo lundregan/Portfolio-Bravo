f3bd8f1e0e8b4f6bbdfddd0a974795f6033c1ba1
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-02-03 16:23:53 +0000

    center on small for blog link text

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -6,9 +6,9 @@
     </div>
     <div class="w-1/2">
       <div v-for="post in posts" :key="post.id" class="bg-gray-100 p-2 shadow-xl text-black my-8">
-        <NuxtLink :to="`blog/${post.link}`">
-          <div class="flex justify-between">
-            <p class="text-blue-700 dark:text-purple-600 text-2xl">{{post.title}}</p>
+        <NuxtLink :to="`blog/${post.link}`" class="text-center lg:text-left">
+          <div class="flex justify-between flex-wrap-reverse">
+            <p class="text-blue-700 dark:text-purple-600 text-2xl mt-2">{{post.title}}</p>
             <p class="italic">{{post.date}}</p>
           </div>
           <p class="p-2">{{post.synopsis}}</p>
