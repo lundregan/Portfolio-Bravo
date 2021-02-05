d26edf52b677669a585974a5fb1c56a6adc1908b
Ethan Lundregan <lundregan.ethan@gmail.com>
2021-02-05 14:34:32 +0000

    global description meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Project-Bravo',
+    title: 'Lundregan',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Ethan Lundregan\'s portfolio website' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
